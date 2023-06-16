83053ea39a940864c24e8795cafff06cbe357383
holy-sinner <holy-sinner@inbox.ru>
2023-06-17 00:21:33 +0300

    create kvadratproverka

---
+++ seminar001_kvadratproverka/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число : ");
+string userInputA = Console.ReadLine() ?? "";
+int userNumberA = int.Parse(userInputA);
+
+Console.Write("Введите число число : ");
+string userInputB = Console.ReadLine() ?? "";
+int userNumberB = int.Parse(userInputB);
+
+if(userNumberA == userNumberB*userNumberB )
+{
+ Console.WriteLine("Да!");
+}
+else
+{
+ Console.WriteLine("Нет((");
+}
+
