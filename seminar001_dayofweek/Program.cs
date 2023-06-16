2e5e55722a1a63aaecd24958815b43f83fc4cc68
holy-sinner <holy-sinner@inbox.ru>
2023-06-17 00:33:15 +0300

    create the day of week programm

---
+++ seminar001_dayofweek/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+
+Console.Write("Введите цифру дня недели  : ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+
+if(userNumber == 1 )
+{
+ Console.WriteLine("Понедельник день тяжёлый((");
+}
+if(userNumber == 2 )
+{
+ Console.WriteLine("Охх, уже вторник");
+}
+if(userNumber == 3 )
+{
+ Console.WriteLine("Среда, почти половина недели позади");
+}
+if(userNumber == 4 )
+{
+ Console.WriteLine("Четверг, ну еще чуть-чуть осталось ");
+}
+if(userNumber == 5 )
+{
+ Console.WriteLine("Ура, пятницв! Надо сегодня нажраться))");
+}
+if(userNumber == 6 )
+{
+ Console.WriteLine("Суббота, ох отосплюсь же я сегодня!");
+}
+if(userNumber == 7 )
+{
+ Console.WriteLine("Воскресенье, люблю воскресенье))");
+}
+else
+{
+ Console.WriteLine("Ты что-то ввёл неверно))");
+}
