ba02eab8d6ece363de8c80095915833f07ec5c64
holy-sinner <holy-sinner@inbox.ru>
2023-06-16 08:31:18 +0300

    add kvadratprogramm

---
+++ seminar001_kvadrat/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите целон число : ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+int result = userNumber*userNumber;
+
+Console.WriteLine($" {userNumber} -> {result}");
+
+
