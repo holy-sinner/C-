6401e68f4e575af88efc9ad38543bd519e943a8a
holy-sinner <holy-sinner@inbox.ru>
2023-06-17 11:10:50 +0300

    create iz3chislabolshee programm

---
+++ dz1seminar_iz3chiselbolshee/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число : ");
+string userInputA = Console.ReadLine() ?? "";
+int a = int.Parse(userInputA);
+
+Console.Write("Введите второе число : ");
+string userInputB = Console.ReadLine() ?? "";
+int b = int.Parse(userInputB);
+
+Console.Write("Введите nhtnmt число : ");
+string userInputC = Console.ReadLine() ?? "";
+int c = int.Parse(userInputC);
+
+int max = a;
+
+if (b > a) max = b;
+if (c > a) max = c;
+
+Console.WriteLine($" {a},{b},{c} -> max = {max}");
+
+
+
+    
+
