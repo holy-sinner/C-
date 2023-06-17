fc63a4170a85b97c1ba86e22cdea1573a4ff0f9d
holy-sinner <holy-sinner@inbox.ru>
2023-06-17 16:26:10 +0300

    add 1 to N programm

---
+++ dz1seminar_1toN/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите число N : ");
+string userInput = Console.ReadLine() ?? "";
+int n = int.Parse(userInput);
+int i = 0;
+Console.Write($"{n}->");
+
+while (i < n-2 )
+{
+    i = i + 1;
+    if (i%2 == 0)
+    Console.Write($"{i},");
+}
+while (i < n )
+{
+    i = i + 1;
+    if (i%2 == 0)
+    Console.Write($"{i}");
+}
