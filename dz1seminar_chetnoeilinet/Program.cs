ad7e0f30e44c3e34d88ec043737d2b71d96b5a9e
holy-sinner <holy-sinner@inbox.ru>
2023-06-17 11:35:18 +0300

    create chetnoeilinet programm

---
+++ dz1seminar_chetnoeilinet/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое число : ");
+string userInputA = Console.ReadLine() ?? "";
+int a = int.Parse(userInputA);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($" {a} -> да");
+}
+else
+{
+    Console.WriteLine($" {a} -> нет");
+}
