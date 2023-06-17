c4d1c3f7314e69eb753c5c431be0d19d213dae6b
holy-sinner <holy-sinner@inbox.ru>
2023-06-17 10:47:48 +0300

    create programm nahozdenie bolshego

---
+++ dz1seminar_nahogdeniebolshego/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число : ");
+string userInputA = Console.ReadLine() ?? "";
+int a = int.Parse(userInputA);
+
+Console.Write("Введите второе число : ");
+string userInputB = Console.ReadLine() ?? "";
+int b = int.Parse(userInputB);
+
+if (a >= b)
+{
+    Console.WriteLine($" a = {a}; b = {b} -> max = {a}");
+}
+if (b > a)
+{
+    Console.WriteLine($" a = {a}; b = {b} -> max = {b}");
+}
