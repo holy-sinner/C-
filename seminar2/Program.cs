62cf89c9e2a941892599783ec484557aadf7883b
holy-sinner <holy-sinner@inbox.ru>
2023-06-20 23:49:59 +0300

    add homework

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*Console.Clear();
 
 Console.WriteLine("Vvedite chislo:");
 string chislo = Console.ReadLine();
@@ -7,4 +9,44 @@ int b = a / 10;
 int c = b % 10;
 
 
-Console.WriteLine($"{a}->{c}");
+Console.WriteLine($"{a}->{c}"); */
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+int a = int.Parse(number);
+if (a < 100)
+{
+    Console.WriteLine($"{a}->третьей цифры нет");
+}
+else
+{
+while (a > 1000)
+    {
+        a = a / 10;
+    }
+Console.WriteLine($"{number}->{a % 10}");
+}
+*/
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*
+Console.WriteLine("Введите цифру дня недели:");
+string number = Console.ReadLine();
+int a = int.Parse(number);
+if ((a < 6 )&&(a > 0))
+{
+    Console.WriteLine($"{a}-> нет");
+}
+if ((a > 5)&&(a < 8) )
+{
+    Console.WriteLine($"{a}-> да");
+}
+if ((a < 1 )&&(a > 7))
+{
+    Console.WriteLine($"{a}-> ошибка вводв");
+}
+*/
