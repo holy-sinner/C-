d701321289c9b8de01837b1bda0220177718dd25
holy-sinner <holy-sinner@inbox.ru>
2023-06-20 21:58:59 +0300

    add seminar2

---
+++ seminar2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Vvedite chislo:");
+string chislo = Console.ReadLine();
+int a = int.Parse(chislo);
+int b = a / 10;
+int c = b % 10;
+
+
+Console.WriteLine($"{a}->{c}");
