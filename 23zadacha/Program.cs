633ed1a36871aebc0018507d8e2d3365097b76cf
holy-sinner <holy-sinner@inbox.ru>
2023-06-22 18:20:14 +0300

    add 23

---
+++ 23zadacha/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+*/
+
+Console.Clear();
+
+int i = 0;
+int number;
+
+try
+{
+    Console.Write("Введите число N: ");
+    number = int.Parse(Console.ReadLine());
+}
+catch (Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+    return;
+}
+
+Console.Write($"{number} -> ");
+
+while (i < number - 1)
+{
+    i = i + 1;
+    Console.Write($"{i * i * i},");
+}
+i = i + 1;
+Console.Write($"{i * i * i}");
