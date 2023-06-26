6bcf44ecb0c3d7a729f587acfeda02c955dcb7db
holy-sinner <holy-sinner@inbox.ru>
2023-06-26 13:11:30 +0300

    add zadacha 29

---
+++ seminares/zadacha29/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+void FillArray(int[] collection)
+
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    
+    while (position < count -1 )
+    {
+        
+        Console.Write($"{col[position]},");
+        position++;
+    }
+    
+    Console.Write($"{col[position]}");
+    Console.Write(" -> ");
+ 
+    count = col.Length;
+    position = 0;
+
+    Console.Write("[");
+        while (position < count -1 )
+    {
+        
+        Console.Write($"{col[position]},");
+        position++;
+    }
+    Console.Write($"{col[position]}]");
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
