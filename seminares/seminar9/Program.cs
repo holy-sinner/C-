88c26e50c1e635f82841c6cf4531de498d0e7cc9
holy-sinner <holy-sinner@inbox.ru>
2023-07-18 22:39:27 +0300

    add 64

---
+++ seminares/seminar9/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+/*
+Console.Write("Введите число M: ");
+
+int m = int.Parse(Console.ReadLine()??"");
+
+Console.Write("Введите число N: ");
+
+int n = int.Parse(Console.ReadLine()??"");
+
+Console.Write($"M = {m} ; N = {n} -> {GetNum(m,n)}");
+
+string GetNum(int start, int end)
+{
+    if(start==end)
+        return start.ToString();
+
+    return start + "," + GetNum(start + 1,n);
+
+} 
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.Write("Введите число : ");
+
+int m = int.Parse(Console.ReadLine()??"");
+
+
+
+Console.Write($"{m} -> {GetNum(m)}");
+
+int GetNum(int num)
+{
+    
+    if(num == 0)
+        return 0;
+    
+    return num % 10 + GetNum( num / 10);
+        
+
+    
+
+}
+ */
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+Console.Write("Введите число A: ");
+
+int a = int.Parse(Console.ReadLine()??"");
+
+Console.Write("Введите число B: ");
+
+int b = int.Parse(Console.ReadLine()??"");
+
+
+
+Console.Write($"A = {a} B = {b}-> {GetNum(a,b)} ({a} в степени {b}) ");
+
+int GetNum(int a, int b)
+{
+    
+    if(b == 0)
+        return 1;
+    
+    return a * GetNum(a, b - 1);
+        
+
+    
+
+}
---
+++ seminares/zadacha64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+Console.Write("Введите число N: ");
+
+int n = int.Parse(Console.ReadLine()??"");
+
+
+
+Console.Write($"N = {n} ->  {GetNum(1,n)}");
+
+
+
+string GetNum(int start, int end)
+{
+    if(end==start)
+        return end.ToString();
+
+    return end + "," + GetNum( 1,end - 1);
+
+} 
